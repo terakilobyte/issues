8f3842d5dddb6d897c26a08a8925d742104619bc
terakilobyte <terakilobyte@gmail.com>
2015-12-14 21:08:14 -0800

    Fix code and tests

--- test/issues_test.exs
+++ test/issues_test.exs
@@ -10,10 +10,10 @@ defmodule IssuesTest do
   end
 
   test "three values returned if three given" do
-    assert parse_args(["user", "project", "99"]) == {"user", "project", "99"}
+    assert parse_args(["user", "project", "99"]) == {"user", "project", 99}
   end
 
   test "count is defaulted if two values are given" do
-    assert parse_args(["user", "project"]) == {"user", "project", "4"}
+    assert parse_args(["user", "project"]) == {"user", "project", 4}
   end
 end
